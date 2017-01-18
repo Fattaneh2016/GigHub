55bfb12290e93a9b4b4914ca54fc4760eb455768
Fattaneh <fatameri@hotmail.com>
2017-01-18 01:24:18 -0500

    add file

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(musicStore.Startup))]
+namespace musicStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
